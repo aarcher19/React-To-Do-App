5502fb3854930d100866bc9e026c04a2bddd80ea
Andrew Archer <Archer.Andrew.J@outlook.com>
2023-08-07 21:59:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  // const [tasks, setTasks] = useState([]);
+
+  // const [newTaskText, setNewTaskText] = useState("");
+
+  // const
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My To-Do List</h1>
       </header>
+      <main>
+        <form>
+          <input type="text"></input>
+          <button type="submit"></button>
+        </form>
+      </main>
     </div>
   );
 }
